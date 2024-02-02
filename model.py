b374666c656451dc29a2816c76d033b96d96bb27
Jourdin Pierre <pierre.jourdin@student-cs.fr>
2024-02-02 14:35:26 +0100

    added time computation

--- model.py
+++ model.py
@@ -2,9 +2,12 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import f1_score
 from sklearn.decomposition import PCA
 from sklearn.pipeline import make_pipeline
+import time
 
 from data import get_train_data
 from features import DATE_FEATURES, COLOR_FEATURES, CHANGE_STATUS_DATE_FEATURES
+
+t0 = time.time()
 
 train_x, train_y, val_x, val_y = get_train_data(["duration", "area", "perimeter"], n_data=-1)
 
@@ -20,3 +23,4 @@ score = f1_score(val_y, pred_y, average="weighted")
 
 print('score = ', score)
 print('pca_components = ', pca_components)
+print('time = ', time.time() - t0)
