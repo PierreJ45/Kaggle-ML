86b6afe8752f0250ca99baf782c07c39b264669c
Jourdin Pierre <pierre.jourdin@student-cs.fr>
2024-02-02 14:10:46 +0100

    small confort changes in model

--- model.py
+++ model.py
@@ -5,12 +5,12 @@ from data import get_train_data
 from features import DATE_FEATURES, COLOR_FEATURES, CHANGE_STATUS_DATE_FEATURES
 
 
-train_x, train_y, test_x, test_y = get_train_data(["duration", "area", "perimeter"] + DATE_FEATURES, n_data=-1)
+train_x, train_y, val_x, val_y = get_train_data(["duration", "area", "perimeter"], n_data=-1)
 
 neigh = KNeighborsClassifier(n_neighbors=5)
 neigh.fit(train_x, train_y)
 
-pred_y = neigh.predict(test_x)
-score = f1_score(test_y, pred_y, average="weighted")
+pred_y = neigh.predict(val_x)
+score = f1_score(val_y, pred_y, average="weighted")
 
-print(score)
+print('score = 'score)
