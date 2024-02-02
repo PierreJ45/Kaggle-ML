f259fdee88f88ee3cac8c46ccde59f54ceb18404
Jourdin Pierre <pierre.jourdin@student-cs.fr>
2024-02-02 14:32:27 +0100

    pca implemented

--- model.py
+++ model.py
@@ -1,16 +1,22 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import f1_score
+from sklearn.decomposition import PCA
+from sklearn.pipeline import make_pipeline
 
 from data import get_train_data
 from features import DATE_FEATURES, COLOR_FEATURES, CHANGE_STATUS_DATE_FEATURES
 
-
 train_x, train_y, val_x, val_y = get_train_data(["duration", "area", "perimeter"], n_data=-1)
 
-neigh = KNeighborsClassifier(n_neighbors=5)
-neigh.fit(train_x, train_y)
+pipeline = make_pipeline(PCA(n_components=2), KNeighborsClassifier(n_neighbors=5))
 
-pred_y = neigh.predict(val_x)
+pipeline.fit(train_x, train_y)
+
+pca_components = pipeline.named_steps['pca'].components_
+
+pred_y = pipeline.predict(val_x)
+
 score = f1_score(val_y, pred_y, average="weighted")
 
-print('score = 'score)
+print('score = ', score)
+print('pca_components = ', pca_components)
