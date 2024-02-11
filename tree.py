b51aef8db2b04c575090a2146c2371a3fb1108a3
Jourdin Pierre <pierre.jourdin@student-cs.fr>
2024-02-11 10:50:24 +0100

    pca in tree

--- tree.py
+++ tree.py
@@ -9,10 +9,13 @@ from tqdm import tqdm
 from scipy.stats import randint
 from sklearn.model_selection import RandomizedSearchCV
 import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.pipeline import make_pipeline
+import time
 
 
 train_x, train_y, test_x, test_y = get_train_data(
-    ["duration", "area", "perimeter", "elongation", "nb_points"] + start_color_features + end_color_features,
+    ["duration", "area", "perimeter", "elongation"] + start_color_features + end_color_features,
     n_data = -1,
     val_size = 0.1
 )
@@ -37,18 +40,21 @@ train_x, train_y, test_x, test_y = get_train_data(
 # best_rf = rand_search.best_estimator_
 # print('Best hyperparameters:',  rand_search.best_params_)
 
-best_rf = RandomForestClassifier(n_estimators=100, max_depth=50, min_samples_split=5, min_samples_leaf=1, random_state=42)
-best_rf.fit(train_x, train_y)
+t0 = time.time()
 
-print(best_rf.predict(test_x)[:5])
+pipeline = make_pipeline(PCA(n_components=10), RandomForestClassifier(n_estimators=100, max_depth=50, min_samples_split=5, min_samples_leaf=1, random_state=42))
 
-score = f1_score(test_y, best_rf.predict(test_x), labels=range(NB_CLASSES), average="weighted")
-train_score = f1_score(train_y, best_rf.predict(train_x), labels=range(NB_CLASSES), average="weighted")
-nb_errors = (best_rf.predict(test_x) != test_y).sum()
-nb_errors_train = (best_rf.predict(train_x) != train_y).sum()
+pipeline.fit(train_x, train_y)
+
+#print(pipeline.predict(test_x)[:5])
+
+score = f1_score(test_y, pipeline.predict(test_x), labels=range(NB_CLASSES), average="weighted")
+train_score = f1_score(train_y, pipeline.predict(train_x), labels=range(NB_CLASSES), average="weighted")
+nb_errors = (pipeline.predict(test_x) != test_y).sum()
+nb_errors_train = (pipeline.predict(train_x) != train_y).sum()
 
 print('F1 score:', score, 'Train F1 score:', train_score, 'Nb errors:', nb_errors / len(test_y), 'Nb errors train:', nb_errors_train / len(train_y))
+print('DataLessTime:', time.time() - t0)
 
-feature_importances = pd.Series(best_rf.feature_importances_, index=train_x.columns).sort_values(ascending=False)
-print(feature_importances)
-
+#feature_importances = pd.Series(pipeline.feature_importances_, index=train_x.columns).sort_values(ascending=False)
+#print(feature_importances)
