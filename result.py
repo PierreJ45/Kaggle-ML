53f0131d7dd6dfa43850e6944be4515d98254154
Aymeric Conti <aymeric.conti2002@gmail.com>
2024-02-12 21:53:33 +1100

    knn not done

--- result.py
+++ result.py
@@ -1,5 +1,9 @@
-def create_result_file(predictions, file_name='result.csv'):
+from tqdm import tqdm
+
+def create_result_file(predictions, indexes: list, file_name='result.csv'):
     with open(file_name, 'w') as f:
         f.write('Id,change_type\n')
-        for i in range(len(predictions)):
-            f.write(f'{i},{predictions[i]}\n')
+        
+        for i in tqdm(range(len(indexes))):
+            index = indexes.index(i)
+            f.write(f"{i},{predictions[index]}\n")
